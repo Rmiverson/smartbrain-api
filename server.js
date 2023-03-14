fe3f19f07f911eaaf1459fec153adf15513b0d72
rmiverson <rmiverson99@gmail.com>
2023-03-14 13:56:39 -0700

    worked on signin and register for lesson 311

--- server.js
+++ server.js
@@ -2,8 +2,54 @@ import express from 'express'
 
 const app = express()
 
+const db = {
+  users: [
+    {
+      id: '123',
+      name: 'john',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '123',
+      name: 'sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('this is working')
+  res.send(db.users)
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === db.users[0].email && req.body.password === db.users[0].password) {
+    res.json('Successs')
+  } else {
+    res.status(400).json('Error loggin in.')
+  }
+  
+})
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body
+  db.users.push({
+      id: '125',
+      name: name,
+      email: email,
+      password: password,
+      entries: 0,
+      joined: new Date()
+  })
+  res.json(db.users[db.users.length - 1])
 })
 
 app.listen(3000, () => {
