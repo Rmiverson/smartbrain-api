1b869f14354d797b7886ccadf3132d07a29e3fb1
rmiverson <rmiverson99@gmail.com>
2023-03-17 13:20:10 -0700

    finished work for lesson 312

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const db = {
   users: [
     {
-      id: '123',
+      id: 123,
       name: 'john',
       email: 'john@gmail.com',
       password: 'cookies',
@@ -13,7 +13,7 @@ const db = {
       joined: new Date()
     },
     {
-      id: '123',
+      id: 124,
       name: 'sally',
       email: 'sally@gmail.com',
       password: 'bananas',
@@ -27,14 +27,14 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send(db.users)
+  return res.send(db.users)
 })
 
 app.post('/signin', (req, res) => {
   if (req.body.email === db.users[0].email && req.body.password === db.users[0].password) {
-    res.json('Successs')
+    return res.json('Successs')
   } else {
-    res.status(400).json('Error loggin in.')
+    return res.status(400).json('Error loggin in.')
   }
   
 })
@@ -49,21 +49,43 @@ app.post('/register', (req, res) => {
       entries: 0,
       joined: new Date()
   })
-  res.json(db.users[db.users.length - 1])
+  
+  return res.json(db.users[db.users.length - 1])
+})
+
+app.get('/profile/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  let found = false
+
+  db.users.forEach(user => {
+    if (user.id === id) {
+      found = true
+      return res.json(user)
+    } 
+  })
+
+  if (!found) {
+    return res.status(400).json('not found')
+  }
+})
+
+app.post('/image', (req, res) => {
+  const { id } = req.body
+  let found = false
+
+  db.users.forEach(user => {
+    if (user.id === id) {
+      found = true
+      user.entries++
+      return res.json(user.entries)
+    } 
+  })
+
+  if (!found) {
+    return res.status(400).json('not found')
+  }
 })
 
 app.listen(3000, () => {
   console.log('all good')
 })
-
-/* 
-
-/ ~~> res = this is working
-
-ideally we always want to have passwords sent in a POST
-/signin ~~> POST = success/fail
-/register ~~> POST = new user
-/profile/:userid ~~> GET = user
-/image ~~> PUT = user  
-
-*/
